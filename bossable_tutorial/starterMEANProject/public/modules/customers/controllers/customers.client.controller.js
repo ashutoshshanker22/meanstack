b40d258cc5a070ea6cb850b73c928813b82191ca
Ashutosh Shanker <ashutoshshanker22@gmail.com>
2015-04-15 21:41:28 -0700

    Hooking up view with controller to update data all the way upto mongodb

--- bossable_tutorial/starterMEANProject/public/modules/customers/controllers/customers.client.controller.js
+++ bossable_tutorial/starterMEANProject/public/modules/customers/controllers/customers.client.controller.js
@@ -17,6 +17,14 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
 				templateUrl: 'modules/customers/views/edit-customer.client.view.html',
 				controller: function ($scope, $modalInstance, customer){
 					$scope.customer = customer;
+
+					$scope.ok = function () {
+						$modalInstance.close($scope.customer);
+					};
+
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+					};
 				},
 				size: size,
 				resolve: {
@@ -45,7 +53,16 @@ customersApp.controller('CustomersCreateController', ['$scope', 'Customers',
 
 customersApp.controller('CustomersUpdateController', ['$scope', 'Customers',
 	function($scope, Customers) {
+		// Update existing Customer
+		this.update = function(updatedCustomer) {
+			var customer = updatedCustomer;
 
+			customer.$update(function() {
+
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
 
 	}
 ]);
